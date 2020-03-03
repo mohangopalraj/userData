fedd6124b5f0facab518ebf85503291eeb9e0fb7
gmdctbe <gmdctbe@gmail.com>
2020-03-03 12:06:10 +0530

    add mailer files

--- userData/app/mailers/user_mailer.rb
+++ userData/app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+	default from: 'test@example.com'
+
+  def welcome(user)
+    @user = user
+    mail(:to => @user.email, :subject => "Welcome!")
+  end
+  
 end
